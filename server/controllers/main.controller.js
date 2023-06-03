1f82059bae237d9c5efc3894f1fd163de34d9ce7
Dark869 <zs21015931@estudiantes.uv.mx>
2023-06-02 21:37:28 -0600

    Actualizacion en el diseño de la pagina

--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -3,15 +3,11 @@ export const getIndexhtml =  (req, res) => {
 };
 
 export const getnicepage = (req, res) => {
-    res.sendFile(process.cwd() + "/client/src/style/nicepage.css");
+    res.sendFile(process.cwd() + "/client/src/styles/nicepage.css");
 };
 
-export const getCasa = (req, res) => {
-    res.sendFile(process.cwd() + "/client/src/style/Casa.css");
-};
-
-export const getLetter = (req, res) => {
-    res.sendFile(process.cwd() + "/client/src/style/letters-style.css");
+export const getPagina1 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/styles/Pagina1.css");
 };
 
 export const getJquery = (req, res) => {
@@ -22,6 +18,34 @@ export const getNicepagejs = (req, res) => {
     res.sendFile(process.cwd() + "/client/src/scripts/nicepage.js");
 };
 
-export const getImgPropiIntelec = (req, res) => {
-    res.sendFile(process.cwd() + "/client/src/images/propiedad-intelectual.jpg");
+export const getImg1 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/images/ImagendeWhatsApp2023-05-04alas00.10.26.jpg");
+};
+
+export const getImg2 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/images/ImagendeWhatsApp2023-05-04alas21.35.59.jpg");
+};
+
+export const getImgCss1 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/images/edfmin1.jpg");
+};
+
+export const getImgCss2 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/images/new.png");
+};
+
+export const getImgCss3 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/images/6203999.jpg");
+};
+
+export const getImgCss4 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/images/cartoon-document-icon-in-comic-style-archive-data-file-sign-illustration-pictogram-document-business-splash-effect-concept-vector.jpg");
+};
+
+export const getImgCss5 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/images/53400251-contacto-o-conexin-hablar-por-telfono-personaje-de-dibujos-animados.webp");
+};
+
+export const getImgCss6 = (req, res) => {
+    res.sendFile(process.cwd() + "/client/src/images/juicio-e1607909165931.jpg");
 };
